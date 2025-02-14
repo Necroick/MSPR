902fe749dad1c306eaf7dc7d3aaa95dac64f620e
Necroick <loik.gourvil@gmail.com>
2025-02-14 17:24:11 +0100

    ajout data 2020

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('raw_data/donnees_communes_2021.csv', sep=';')
-df.rename(columns={'COM': 'Code Postale', 'PMUN': 'Population municipale', 'PCAP': 'Population comptée à part', 'PTOT': 'Population totale'}, inplace=True)
+df_2021 = pd.read_csv('raw_data/donnees_communes_2021.csv', sep=';')
+df_2021.rename(columns={'COM': 'Code Postale', 'PMUN': 'Population municipale', 'PCAP': 'Population comptée à part', 'PTOT': 'Population totale'}, inplace=True)
 
-print(df.columns)
+df_2020 = pd.read_csv('raw_data/donnees_communes_2020.csv', sep=';')
+df_2020.rename(columns={'COM': 'Code Postale', 'PMUN': 'Population municipale', 'PCAP': 'Population comptée à part', 'PTOT': 'Population totale'}, inplace=True)
