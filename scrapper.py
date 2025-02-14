c2514c3ced8398ee7bdc71ca6b81aed5d49b84d6
Necroick <loik.gourvil@gmail.com>
2025-02-14 16:58:00 +0100

    ajout raw data

---
+++ scrapper.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df1 = pd.read_excel('raw_data/Pop_age_moyen_median.xlsx')
+df2 = pd.read_excel('raw_data/Pop_age_moyen_median.xlsx')
+df3 = pd.read_excel('raw_data/Pop_age_moyen_median.xlsx')
+df4 = pd.read_excel('raw_data/Pop_age_moyen_median.xlsx')
