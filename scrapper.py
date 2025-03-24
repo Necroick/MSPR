9b484d804067e1e797a2ccb2b899aa985c3270c6
LOICK GOURVIL <loick.gourvil@laposte.fr>
2025-03-24 15:06:36 +0100

    rename populations files

--- scrapper.py
+++ scrapper.py
@@ -2,30 +2,30 @@ from scrapper_function import createDepcom
 import pandas as pd
 
 # Ouverture des données de 2022 // Renommage de certaines colonnes pour une cohérence entre les datasets
-df_2022 = pd.read_csv('raw_data/donnees_communes_2022.csv', sep=';')
+df_2022 = pd.read_csv('raw_data/donnees_population_communes_2022.csv', sep=';')
 df_2022.rename(columns={'COM':'DEPCOM', 'REG':'CODREG', 'DEP':'CODDEP', 'PMUN': 'Population municipale', 'PCAP': 'Population comptée à part', 'PTOT': 'Population totale'}, inplace=True)
 
 # Ouverture des données de 2021 // Renommage de certaines colonnes pour une cohérence entre les datasets
-df_2021 = pd.read_csv('raw_data/donnees_communes_2021.csv', sep=';')
+df_2021 = pd.read_csv('raw_data/donnees_population_communes_2021.csv', sep=';')
 df_2021.rename(columns={'COM':'DEPCOM', 'REG':'CODREG', 'DEP':'CODDEP', 'PMUN': 'Population municipale', 'PCAP': 'Population comptée à part', 'PTOT': 'Population totale'}, inplace=True)
 
 # Ouverture des données de 2020 // Renommage de certaines colonnes pour une cohérence entre les datasets
-df_2020 = pd.read_csv('raw_data/donnees_communes_2020.csv', sep=';')
+df_2020 = pd.read_csv('raw_data/donnees_population_communes_2020.csv', sep=';')
 df_2020.rename(columns={'REG':'Région', 'COM': 'Commune', 'PMUN': 'Population municipale', 'PCAP': 'Population comptée à part', 'PTOT': 'Population totale'}, inplace=True)
 df_2020 = createDepcom(df_2020)
 
 # Ouverture des données de 2019 // Renommage de certaines colonnes pour une cohérence entre les datasets
-df_2019 = pd.read_csv('raw_data/donnees_communes_2019.csv', sep=';')
+df_2019 = pd.read_csv('raw_data/donnees_population_communes_2019.csv', sep=';')
 df_2019.rename(columns={'REG':'Région', 'COM': 'Commune', 'PMUN': 'Population municipale', 'PCAP': 'Population comptée à part', 'PTOT': 'Population totale'}, inplace=True)
 df_2019 = createDepcom(df_2019)
 
 # Ouverture des données de 2018 // Renommage de certaines colonnes pour une cohérence entre les datasets
-df_2018 = pd.read_csv('raw_data/donnees_communes_2018.csv', sep=';')
+df_2018 = pd.read_csv('raw_data/donnees_population_communes_2018.csv', sep=';')
 df_2018.rename(columns={'REG':'Région', 'COM': 'Commune', 'PMUN': 'Population municipale', 'PCAP': 'Population comptée à part', 'PTOT': 'Population totale'}, inplace=True)
 df_2018 = createDepcom(df_2018)
 
 # Ouverture des données de 2017 // Ajout et renommage de certaines colonnes pour une cohérence entre les datasets
-df_2017 = pd.read_csv('raw_data/donnees_communes_2017.csv', sep=';')
+df_2017 = pd.read_csv('raw_data/donnees_population_communes_2017.csv', sep=';')
 df_2017.rename(columns={'COM': 'Commune', 'PMUN': 'Population municipale', 'PCAP': 'Population comptée à part', 'PTOT': 'Population totale'}, inplace=True)
 df_2017['CODDEP'] = df_2017['DEPCOM'].str[:2]
 df_2017['CODCOM'] = df_2017['DEPCOM'].str[2:]
