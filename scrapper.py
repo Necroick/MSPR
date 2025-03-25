7ee4c5c3a9239e0bdabc1637f45d332a19d0984d
LOICK GOURVIL <loick.gourvil@laposte.fr>
2025-03-25 10:28:27 +0100

    ajout cleaner 2022

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,4 @@
-from scrapper_function import createDepcom
+from scrapper_function import createDepcom, resElectionClean2017, resElectionClean2022
 import pandas as pd
 
 # Ouverture des données de population de 2022 // Renommage de certaines colonnes pour une cohérence entre les datasets
@@ -36,24 +36,13 @@ df_pop_2017 = pd.merge(df_pop_2017, df_pop_2021_subset, on='DEPCOM', how='left')
 
 # Ouverture des données d'élections de 2017 \\ Traitement de .csv pour avoir un candidat et une commun par ligne
 df_elec_2017 = pd.read_csv('raw_data/resultat_election_2017_burvot.csv', sep=';', encoding='latin-1', header=None)
+df_elec_2017 = resElectionClean2017(df_elec_2017) 
+print(df_elec_2017)
 
-# Création des entête manuellement
-base_columns = "Code du département;Libellé du département;Code de la circonscription;Libellé de la circonscription;Code de la commune;Libellé de la commune;Code du b.vote;Inscrits;Abstentions;% Abs/Ins;Votants;% Vot/Ins;Blancs;% Blancs/Ins;% Blancs/Vot;Nuls;% Nuls/Ins;% Nuls/Vot;Exprimés;% Exp/Ins;% Exp/Vot".split(';')
-candidat_columns = "N°Panneau;Sexe;Nom;Prénom;Voix;% Voix/Ins;% Voix/Exp".split(';')
-candidat_columns_renamed = []
-for i in range(11):
-    candidat_columns_renamed.extend([f'{col}_candi{i+1}' for col in candidat_columns])
-df_elec_2017.columns = base_columns+candidat_columns_renamed
-
-# Suppression des entête inutiles
-df_elec_2017.drop(columns=['Code de la circonscription','Libellé de la circonscription','Code du b.vote','% Abs/Ins', '% Vot/Ins', '% Blancs/Ins', '% Blancs/Vot', '% Nuls/Ins', '% Nuls/Vot', '% Exp/Ins', '% Exp/Vot', '% Voix/Ins_candi1', '% Voix/Exp_candi1', '% Voix/Ins_candi2', '% Voix/Exp_candi2', '% Voix/Ins_candi3', '% Voix/Exp_candi3', '% Voix/Ins_candi4', '% Voix/Exp_candi4', '% Voix/Ins_candi5', '% Voix/Exp_candi5', '% Voix/Ins_candi6', '% Voix/Exp_candi6', '% Voix/Ins_candi7', '% Voix/Exp_candi7', '% Voix/Ins_candi8', '% Voix/Exp_candi8', '% Voix/Ins_candi9', '% Voix/Exp_candi9', '% Voix/Ins_candi10', '% Voix/Exp_candi10', '% Voix/Ins_candi11', '% Voix/Exp_candi11'], inplace=True)
-
-# Regroupper les données par communes
-num_col = ['Inscrits','Abstentions','Votants','Blancs','Nuls','Exprimés','Voix_candi1','Voix_candi2','Voix_candi3','Voix_candi4','Voix_candi5','Voix_candi6','Voix_candi7','Voix_candi8','Voix_candi9','Voix_candi10','Voix_candi11']
-group_col = [col for col in df_elec_2017.columns if col not in num_col]
-df_elec_2017 = df_elec_2017.groupby(group_col, as_index=False).sum()
-
-print(df_elec_2017.head(10))
+# Ouverture des données d'élections de 2022 \\ Traitement de .csv pour avoir un candidat et une commun par ligne
+df_elec_2022 = pd.read_csv('raw_data/resultat_election_2022_burvot.csv', sep=';', encoding='latin-1', header=None)
+df_elec_2022 = resElectionClean2022(df_elec_2022)
+print(df_elec_2022)
 
 """
 print(df_pop_2017)
